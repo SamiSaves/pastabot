b0bad165c1d98247e5dbe22151315d3399ef9133
Sami Saves <sami.saves@reaktor.com>
2021-09-17 13:59:47 +0300

    Revert "fix"
    This reverts commit cbca3b2bb89cad6546a820477f854f50ea8c231c.

--- api/pasta.ts
+++ api/pasta.ts
@@ -17,14 +17,14 @@ const createSlackMessage = (pasta: string) => ({
 });
 
 export default async (request: NowRequest, response: NowResponse) => {
-  const { text: pastaQuery, user_name} = request.query;
+  const { text: pastaQuery } = request.query;
   const pastas = (await fs.readdir(`${__dirname}/../pastas`)).map((fileName) => fileName.replace(/\.[^/.]+$/, ""));
   if (isValidQuery(pastaQuery)) {
     if (isValidPasta(pastas, pastaQuery)) {
       try {
         const pastaFile = await fs.readFile(`${__dirname}/../pastas/${pastaQuery}.txt`, { encoding: "utf-8" });
-        await got((process.env.WEBHOOK_URL as string), {method: 'POST', body: JSON.stringify({pasta: pastaFile + `\n\nTerveisin: ${user_name}`})})
-        return response.status(200).send('')
+        await got((process.env.WEBHOOK_URL as string), {method: 'POST', body: JSON.stringify({pasta: pastaFile})})
+        return response.status(200)
       } catch (error) {
         return response.status(500).send(error.message);
       }
